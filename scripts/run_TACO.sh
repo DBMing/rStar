a878247007bc6cd7ef02dea06faad428ae85630f
DMMing <2577786714@qq.com>
2024-12-02 10:29:35 +0800

    add some TACO dataset

--- scripts/run_TACO.sh
+++ scripts/run_TACO.sh
@@ -1,7 +1,7 @@
 CUDA_VISIBLE_DEVICES=0 python run_src/do_generate.py \
     --dataset_name TACO \
     --test_json_filename test_one \
-    --model_ckpt Qwen/Qwen2.5-Coder-32B \
+    --model_ckpt /root/shared-nvme/rStar/mistralai/Mistral-7B-v0.1 \
     --note default \
     --num_rollouts 3 \
     --verbose \
